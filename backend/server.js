165778b2d775a8ec398ac9231eb48243215c8b7c
shreyansh jaiswal <146716527+shreyanshjais22@users.noreply.github.com>
2026-03-30 22:24:25 +0530

    Update CORS origin for Stripe webhooks

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use(cookieParser());
 
 app.post('/stripe', express.raw({type: 'application/json'}),stripeWebhooks);
 app.use(cors({
-    origin: 'https://liceria-git-main-shreyanshjazz-gmailcoms-projects.vercel.app',
+    origin: 'https://liceria-lyart.vercel.app',
     credentials: true,}
 ));
 
